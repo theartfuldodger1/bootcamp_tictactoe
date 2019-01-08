7a6a37492029b314340a873674619ce52a16896a
theartfuldodger1 <cordellhurst@gmail.com>
2019-01-07 21:48:57 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+            { /* TODO */}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    
+}
